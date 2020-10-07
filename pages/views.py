55bcd86a3106d716cbd37eb260204d84407b0fa7
nmorono <nicolas.morono@gmail.com>
2020-10-07 20:29:45 -0300

    Second cap commit

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,9 @@
 # pages/views.py
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView): # new
+    template_name = 'about.html'
